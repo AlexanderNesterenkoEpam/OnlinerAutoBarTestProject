754e5b0ec6576d62bc62fdf66b89b717084fb44b
Aliaksandr Nestsiarenka1 <Aliaksandr_Nestsiarenka1@epam.com>
2019-10-24 17:01:06 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace OnlinerAutoBarTestProject
+{
+    [TestFixture]
+    public class Class1
+    {
+        DriverCreator dr;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dr = new DriverCreator();
+        }
+
+        /// <summary>
+        /// Summary Test Case 1
+        /// </summary>
+        [Test]
+        public void TestAutoTitleTest()
+        {
+            const string ExpectedTitle = "Автобарахолка";
+
+            var driver = dr.GetDriver();
+
+            driver.Navigate().GoToUrl("https://ab.onliner.by");
+
+            driver.Manage().Window.Maximize();
+
+            string titleText = driver.FindElement(By.XPath("//h1[contains(@class, 'm-title')]")).Text;
+
+            Assert.AreEqual(ExpectedTitle, titleText);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            dr.QuitDriver();
+        }
+    }
+}
---
+++ DriverCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace OnlinerAutoBarTestProject
+{
+    public class DriverCreator
+    {
+        ChromeDriver driver;
+
+        public ChromeDriver GetDriver()
+        {
+            double timeOutSec = 15;
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            var service = ChromeDriverService.CreateDefaultService();
+            var option = new ChromeOptions();
+            option.AddArgument("disable-infobars");
+            option.AddArgument("--no-sandbox");
+            driver = new ChromeDriver(service, option, TimeSpan.FromSeconds(timeOutSec));
+
+            return driver;
+        }
+
+        public void QuitDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
